a98b66efde2b647ffd2086cc79f18683aa6ce6eb
ARazaMaths <adilraza9000@gmail.com>
2024-10-21 23:31:13 +0100

    Added spellcheck and correction

--- .config/nvim/lua/adil/core/options.lua
+++ .config/nvim/lua/adil/core/options.lua
@@ -10,7 +10,9 @@ opt.shiftwidth = 2
 opt.signcolumn = "yes:1"
 opt.clipboard = "unnamedplus"
 opt.conceallevel = 1
+opt.spell = true
 opt.spelllang = en_gb
 opt.autochdir = true
 vim.wo.relativenumber = true
 vim.wo.number = true
+vim.api.nvim_set_keymap("i", "<C-l>", "<c-g>u<Esc>[s1z=`]a<c-g>u", { noremap = true })
