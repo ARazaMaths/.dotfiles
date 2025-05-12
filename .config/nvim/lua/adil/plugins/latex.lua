99d82e38cae3d079719b3a151d3ba035a3d6a317
Adil Raza <adilraza9000@gmail.com>
2025-05-12 14:50:00 +0100

    minor fixes

--- .config/nvim/lua/adil/plugins/latex.lua
+++ .config/nvim/lua/adil/plugins/latex.lua
@@ -3,9 +3,11 @@ return {
 		"lervag/vimtex",
 		lazy = false,
 		init = function()
-			vim.g.vimtex_view_method = "zathura_simple"
+			vim.g.vimtex_view_method = "zathura"
+			vim.g.latex_view_general_viewer = "zathura"
 			vim.g.vimtex_quickfix_mode = 0
 			vim.g.vimtex_matchparen_enable = 0
+			vim.g.vimtex_compiler_progname = "nvr"
 		end,
 	},
 
